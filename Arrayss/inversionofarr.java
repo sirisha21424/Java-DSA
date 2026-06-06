0b8b65be0c2a7b37891a5137c27ac3b88bd56330
Sirisha <katurisirisha9@gmail.com>
2026-06-06 11:50:34 +0530

    Daily update

---
+++ Arrayss/inversionofarr.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long numberOfInversions(int[] nums) {
+
+        long count = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] > nums[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
