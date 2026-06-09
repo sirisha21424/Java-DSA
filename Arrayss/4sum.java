2126a32bb17a4c73182157c85128f983c29f362d
Sirisha <katurisirisha9@gmail.com>
2026-06-09 14:37:22 +0530

    added 4sum problem

---
+++ Arrayss/4sum.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {   if(i>0 && nums[i]==nums[i-1])
+            {   continue;
+            }
+            for(int j=i+1;j<n;j++)
+            {   if(j!=i+1 && nums[j]==nums[j-1])
+                {   continue;
+                }
+                int k=j+1;
+                int l=n-1;
+                while(k<l)
+                {   long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum==target)
+                    {   List<Integer> set=new ArrayList<>();
+                        set.add(nums[i]);
+                        set.add(nums[j]);
+                        set.add(nums[k]);
+                        set.add(nums[l]);
+                        list.add(set);
+                         k++;
+                         l--;
+                        while(k<l && nums[k]==nums[k-1])
+                        {   k++;
+                        }
+                        while(k<l && nums[l]==nums[l+1])
+                        {   l--;
+                        }
+                       
+                    }
+                    else if(sum>target)
+                    {   l--;
+                    }
+                    else
+                    {   k++;
+                    }
+                    
+                }
+            }
+        }
+        return list;
+        
+    }
+}
