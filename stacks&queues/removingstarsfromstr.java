249a483b149687603f9b524b1acecdc97fdfdbdf
Sirisha <katurisirisha9@gmail.com>
2026-06-11 14:48:26 +0530

    added removing stars from a str problem

---
+++ stacks&queues/removingstarsfromstr.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {   if(st.size()>=1 && s.charAt(i)=='*')
+            {   st.pop();
+            }
+            else
+            {   st.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char ch:st)
+        {   sb.append(ch);
+        }
+        return sb.toString();
+
+        
+    }
+}
