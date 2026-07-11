ef6bf614843a66bab39bcf2e5a1d6b41a3020abf
Sirisha <katurisirisha9@gmail.com>
2026-07-11 20:13:24 +0530

    added singlelinkedlist problem

---
+++ Linkedlist/reverseLL(iterative).java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode temp=null;
+        while(curr!=null)
+        {   temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+        
+    }
+}
