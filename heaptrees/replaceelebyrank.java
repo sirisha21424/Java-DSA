4ae3e1e3a2efeb5c7b09f44e8544b8e45deeaad7
Sirisha <katurisirisha9@gmail.com>
2026-08-19 21:37:48 +0530

    sloved replace ele by rank

--- heaptrees/replaceelebyrank.java
+++ heaptrees/replaceelebyrank.java
@@ -2,13 +2,23 @@ import java.util.*;
 class Solution {
     public List<Integer> replaceWithRank(List<Integer> arr) {
         // Your code goes here
-        intn=arr.size();
+        int n=arr.size();
         int[] arr1=new int[n];
         for(int i=0;i<n;i++)
-        {   arr1[i]=list.get(i);
+        {   arr1[i]=arr.get(i);
         }
         Arrays.sort(arr1);
         HashMap<Integer,Integer> map=new HashMap<>();
-        for(inti=0;)
+        int rank=0;
+        for(int i=0;i<n;i++)
+        {   if(!map.containsKey(arr1[i]))
+            {   map.put(arr1[i],++rank);
+            }
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {   list.add(map.get(arr.get(i)));
+        }
+        return list;
     }
 }
