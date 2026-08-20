7e2f1adf24f304c2cfedf2d0513355631fa81fd2
Sirisha <katurisirisha9@gmail.com>
2026-08-20 19:48:50 +0530

    sloved minheap

---
+++ heaptrees/minheap.java
@@ -0,0 +1,44 @@
+class Solution {
+    PriorityQueue<Integer> pq;
+
+    public void initializeHeap() {
+        pq=new PriorityQueue<>();
+        
+    }
+
+    public void insert(int key) {
+        pq.offer(key);
+    }
+
+    public void changeKey(int index, int newVal) {
+        List<Integer> list=new ArrayList<>(pq);
+        if(index<0 || index>pq.size())
+            return ;
+        list.set(index,newVal);
+        pq.clear();
+        pq.addAll(list);
+
+        
+    }
+
+    public void extractMin() {
+        if(pq.size()!=0)
+            pq.poll();
+        
+    }
+
+    public boolean isEmpty() {
+        return pq.isEmpty();
+        
+    }
+
+    public int getMin() {
+        return pq.peek();
+       
+    }
+
+    public int heapSize() {
+        return pq.size();
+        
+    }
+}
