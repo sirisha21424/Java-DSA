fe5e7da750cbbdaffdac4aa34bf344480d15746c
Sirisha <katurisirisha9@gmail.com>
2026-08-30 21:20:07 +0530

    sloved merge ksorted lists

---
+++ Linkedlist/mergeksortedlists.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<lists.length;i++)
+        {   ListNode curr=lists[i];
+            while(curr!=null)
+            {
+                pq.offer(curr.val);
+                curr=curr.next;
+            }
+        }
+        if (pq.isEmpty()) {
+            return null;
+        }
+        ListNode root=new ListNode(pq.poll());
+        ListNode curr1=root;
+        while(pq.size()>0)
+        {   curr1.next=new ListNode(pq.poll());
+            curr1=curr1.next;
+        }
+        return root;
+
+
+        
+    }
+}
