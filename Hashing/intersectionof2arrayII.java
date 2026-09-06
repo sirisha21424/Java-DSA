5567358cbea96442190d82a5ba8e9331582cd779
Sirisha <katurisirisha9@gmail.com>
2026-09-06 20:25:37 +0530

    sloved intersection of 2 arrays

---
+++ Hashing/intersectionof2arrayII.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums1.length;i++)
+        {   map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<nums2.length;i++)
+        {   if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0)
+            {   list.add(nums2[i]);
+                map.put(nums2[i],map.get(nums2[i])-1);
+            }
+        }
+        int n=list.size();
+        int[] arr=new int[n];
+        for(int i=0;i<list.size();i++)
+        {   arr[i]=list.get(i);
+        }
+        return arr;
+
+
+        
+    }
+}
