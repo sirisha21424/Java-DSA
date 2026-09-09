df251e3177fc2d52786266a92ccf2a2843ec6e2f
Sirisha <katurisirisha9@gmail.com>
2026-09-09 20:53:01 +0530

    sloved longest consecutive sequence

---
+++ Hashing/longestconseq.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        long longest = 0;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int length = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    length++;
+                }
+
+                longest = Math.max(longest, length);
+            }
+        }
+
+        return (int)longest;
+    }
+}
