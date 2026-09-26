4dd627bebca4e3cf8c55862954b8a308b4c65b1c
Sirisha <katurisirisha9@gmail.com>
2026-09-26 21:11:15 +0530

    sloved island perimeter

---
+++ Graphs/islandperimeter.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int perimeter=0;
+        for(int i=0;i<n;i++)
+        {   for(int j=0;j<m;j++)
+            {   if(grid[i][j]==1)
+                {   perimeter+=4;
+                    if(i>0 && grid[i-1][j]==1)
+                    {   perimeter--;
+                    }
+                    if(i<n-1 && grid[i+1][j]==1)
+                    {   perimeter--;
+                    }
+                    if(j>0 && grid[i][j-1]==1)
+                    {   perimeter--;
+                    }
+                    if(j<m-1 && grid[i][j+1]==1)
+                    {   perimeter--;
+                    }
+                }
+            }
+        }
+        return perimeter;
+        
+        
+    }
+}
